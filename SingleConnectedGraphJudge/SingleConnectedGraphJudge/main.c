2e30226fecd39e3b52dfec27d4977de027a85987
Rolo <rolo_lamperouge@foxmail.com>
2016-05-20 15:23:53 +0800

    build project

---
+++ SingleConnectedGraphJudge/SingleConnectedGraphJudge/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SingleConnectedGraphJudge
+//
+//  Created by Rolo on 16/5/20.
+//  Copyright © 2016年 Rolo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
